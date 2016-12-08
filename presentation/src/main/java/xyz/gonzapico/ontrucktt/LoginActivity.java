5cb2d543f896ef3597a6c3fe2c978b2fe659e71e
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2016-12-08 20:01:25 +0100

    Login is implemented with MVP from now on

--- presentation/src/main/java/xyz/gonzapico/ontrucktt/LoginActivity.java
+++ presentation/src/main/java/xyz/gonzapico/ontrucktt/LoginActivity.java
@@ -17,35 +17,42 @@
 package xyz.gonzapico.ontrucktt;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import butterknife.BindView;
+import butterknife.OnClick;
 import com.google.firebase.auth.FirebaseUser;
+import xyz.gonzapico.ontrucktt.login.LoginPresenter;
+import xyz.gonzapico.ontrucktt.login.LoginView;
 
-public class LoginActivity extends BaseOTActivity implements View.OnClickListener {
+public class LoginActivity extends BaseOTActivity implements LoginView {
 
-  private static final String TAG = "EmailPassword";
+  @BindView(R.id.status) TextView mStatusTextView;
+  @BindView(R.id.detail) TextView mDetailTextView;
+  @BindView(R.id.email_sign_in_button) Button btnSignIn;
+  @BindView(R.id.email_create_account_button) Button btnCreateAccount;
+  @BindView(R.id.sign_out_button) Button btnSignOut;
+  @BindView(R.id.email_password_buttons) LinearLayout llEmailPasswordButtons;
+  @BindView(R.id.email_password_fields) LinearLayout llEmailPasswordFields;
+  @BindView(R.id.field_email) EditText mEmailField;
+  @BindView(R.id.field_password) EditText mPasswordField;
+  private LoginPresenter mLoginPresenter;
 
-  private TextView mStatusTextView;
-  private TextView mDetailTextView;
-  private EditText mEmailField;
-  private EditText mPasswordField;
+  @OnClick(R.id.email_sign_in_button) void signIn() {
+    mLoginPresenter.signIn(mEmailField, mPasswordField);
+  }
 
-  // [START declare_auth]
-  private FirebaseAuth mAuth;
-  // [END declare_auth]
+  @OnClick(R.id.email_create_account_button) void createAccount() {
+    mLoginPresenter.createAccount(mEmailField, mPasswordField);
+  }
 
-  // [START declare_auth_listener]
-  private FirebaseAuth.AuthStateListener mAuthListener;
-  // [END declare_auth_listener]
+  @OnClick(R.id.sign_out_button) void signOut() {
+    mLoginPresenter.signOut();
+  }
 
   @Override protected int getLayoutResource() {
     return R.layout.activity_emailpassword;
@@ -53,176 +60,51 @@ public class LoginActivity extends BaseOTActivity implements View.OnClickListene
 
   @Override public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_emailpassword);
 
-    // Views
-    mStatusTextView = (TextView) findViewById(R.id.status);
-    mDetailTextView = (TextView) findViewById(R.id.detail);
-    mEmailField = (EditText) findViewById(R.id.field_email);
-    mPasswordField = (EditText) findViewById(R.id.field_password);
-
-    // Buttons
-    findViewById(R.id.email_sign_in_button).setOnClickListener(this);
-    findViewById(R.id.email_create_account_button).setOnClickListener(this);
-    findViewById(R.id.sign_out_button).setOnClickListener(this);
-
-    // [START initialize_auth]
-    mAuth = FirebaseAuth.getInstance();
-    // [END initialize_auth]
-
-    // [START auth_state_listener]
-    mAuthListener = new FirebaseAuth.AuthStateListener() {
-      @Override public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-        if (user != null) {
-          // User is signed in
-          Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-        } else {
-          // User is signed out
-          Log.d(TAG, "onAuthStateChanged:signed_out");
-        }
-        // [START_EXCLUDE]
-        updateUI(user);
-        // [END_EXCLUDE]
-      }
-    };
-    // [END auth_state_listener]
+    mLoginPresenter = new LoginPresenter(this);
   }
 
-  // [START on_start_add_listener]
   @Override public void onStart() {
     super.onStart();
-    mAuth.addAuthStateListener(mAuthListener);
+    mLoginPresenter.onViewAttached();
   }
-  // [END on_start_add_listener]
 
-  // [START on_stop_remove_listener]
   @Override public void onStop() {
     super.onStop();
-    if (mAuthListener != null) {
-      mAuth.removeAuthStateListener(mAuthListener);
-    }
-  }
-  // [END on_stop_remove_listener]
-
-  private void createAccount(String email, String password) {
-    Log.d(TAG, "createAccount:" + email);
-    if (!validateForm()) {
-      return;
-    }
-
-    //showProgressDialog();
-
-    // [START create_user_with_email]
-    mAuth.createUserWithEmailAndPassword(email, password)
-        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-          @Override public void onComplete(@NonNull Task<AuthResult> task) {
-            Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
-
-            // If sign in fails, display a message to the user. If sign in succeeds
-            // the auth state listener will be notified and logic to handle the
-            // signed in user can be handled in the listener.
-            if (!task.isSuccessful()) {
-              Toast.makeText(LoginActivity.this, R.string.auth_failed, Toast.LENGTH_SHORT)
-                  .show();
-            }
-
-            // [START_EXCLUDE]
-            //hideProgressDialog();
-            // [END_EXCLUDE]
-          }
-        });
-    // [END create_user_with_email]
+    mLoginPresenter.onViewDetached();
   }
 
-  private void signIn(String email, String password) {
-    Log.d(TAG, "signIn:" + email);
-    if (!validateForm()) {
-      return;
-    }
+  @Override public void showLoading() {
 
-    //showProgressDialog();
-
-    // [START sign_in_with_email]
-    mAuth.signInWithEmailAndPassword(email, password)
-        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-          @Override public void onComplete(@NonNull Task<AuthResult> task) {
-            Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
-
-            // If sign in fails, display a message to the user. If sign in succeeds
-            // the auth state listener will be notified and logic to handle the
-            // signed in user can be handled in the listener.
-            if (!task.isSuccessful()) {
-              Log.w(TAG, "signInWithEmail:failed", task.getException());
-              Toast.makeText(LoginActivity.this, R.string.auth_failed, Toast.LENGTH_SHORT)
-                  .show();
-            }
-
-            // [START_EXCLUDE]
-            if (!task.isSuccessful()) {
-              mStatusTextView.setText(R.string.auth_failed);
-            }
-            //hideProgressDialog();
-            // [END_EXCLUDE]
-          }
-        });
-    // [END sign_in_with_email]
   }
 
-  private void signOut() {
-    mAuth.signOut();
-    updateUI(null);
+  @Override public void hideLoading() {
+
   }
 
-  private boolean validateForm() {
-    boolean valid = true;
+  @Override public void userLoggedIn(FirebaseUser user) {
+    mStatusTextView.setText(getString(R.string.emailpassword_status_fmt, user.getEmail()));
+    mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
 
-    String email = mEmailField.getText().toString();
-    if (TextUtils.isEmpty(email)) {
-      mEmailField.setError("Required.");
-      valid = false;
-    } else {
-      mEmailField.setError(null);
-    }
-
-    String password = mPasswordField.getText().toString();
-    if (TextUtils.isEmpty(password)) {
-      mPasswordField.setError("Required.");
-      valid = false;
-    } else {
-      mPasswordField.setError(null);
-    }
-
-    return valid;
+    llEmailPasswordButtons.setVisibility(View.GONE);
+    llEmailPasswordFields.setVisibility(View.GONE);
+    btnSignOut.setVisibility(View.VISIBLE);
   }
 
-  private void updateUI(FirebaseUser user) {
-    //hideProgressDialog();
-    if (user != null) {
-      mStatusTextView.setText(getString(R.string.emailpassword_status_fmt, user.getEmail()));
-      mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+  @Override public void userLoggedOut() {
+    mStatusTextView.setText(R.string.signed_out);
+    mDetailTextView.setText(null);
 
-      findViewById(R.id.email_password_buttons).setVisibility(View.GONE);
-      findViewById(R.id.email_password_fields).setVisibility(View.GONE);
-      findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
-    } else {
-      mStatusTextView.setText(R.string.signed_out);
-      mDetailTextView.setText(null);
-
-      findViewById(R.id.email_password_buttons).setVisibility(View.VISIBLE);
-      findViewById(R.id.email_password_fields).setVisibility(View.VISIBLE);
-      findViewById(R.id.sign_out_button).setVisibility(View.GONE);
-    }
+    llEmailPasswordButtons.setVisibility(View.VISIBLE);
+    llEmailPasswordFields.setVisibility(View.VISIBLE);
+    btnSignOut.setVisibility(View.GONE);
   }
 
-  @Override public void onClick(View v) {
-    int i = v.getId();
-    if (i == R.id.email_create_account_button) {
-      createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
-    } else if (i == R.id.email_sign_in_button) {
-      signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
-    } else if (i == R.id.sign_out_button) {
-      signOut();
-    }
+  @Override public void showAuthFailed() {
+    Toast.makeText(this, R.string.auth_failed, Toast.LENGTH_SHORT).show();
+  }
+
+  @Override public void showStatusAuthFailed() {
+    mStatusTextView.setText(R.string.auth_failed);
   }
 }
