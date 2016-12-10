e0ea7ec1a2183b9ba3a6496570234ecccf2023ce
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2016-12-10 12:48:07 +0100

    Split the result of showing loads and user into two different screens

--- presentation/src/main/java/xyz/gonzapico/ontrucktt/MainActivity.java
+++ presentation/src/main/java/xyz/gonzapico/ontrucktt/MainActivity.java
@@ -1,72 +1,31 @@
 package xyz.gonzapico.ontrucktt;
 
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import butterknife.BindView;
-import java.util.List;
-import xyz.gonzapico.ontrucktt.showLoads.Load;
-import xyz.gonzapico.ontrucktt.showLoads.ShowLoadsPresenter;
-import xyz.gonzapico.ontrucktt.showLoads.ShowLoadsView;
-import xyz.gonzapico.ontrucktt.showLoads.adapter.LoadsAdapter;
-import xyz.gonzapico.ontrucktt.showUsers.ShowUsersPresenter;
-import xyz.gonzapico.ontrucktt.showUsers.ShowUsersView;
-import xyz.gonzapico.ontrucktt.showUsers.User;
-import xyz.gonzapico.ontrucktt.showUsers.adapter.UsersAdapter;
+import butterknife.OnClick;
+import xyz.gonzapico.ontrucktt.navigator.Navigator;
 
-public class MainActivity extends BaseOTActivity implements ShowUsersView, ShowLoadsView {
+public class MainActivity extends BaseOTActivity {
+  private Navigator mNavigator;
 
-  @BindView(R.id.rvUsers) RecyclerView rvUsers;
-  @BindView(R.id.rvLoads) RecyclerView rvLoads;
-  private ShowUsersPresenter showUsersPresenter;
-  private ShowLoadsPresenter showLoadsPresenter;
+  @OnClick(R.id.btnViewUsers) void viewUsers() {
+    mNavigator.navigateToUsers(this);
+  }
+
+  @OnClick(R.id.btnViewLoads) void viewLoads() {
+    mNavigator.navigateToLoads(this);
+  }
+
+  @OnClick(R.id.btnLogin) void login() {
+    mNavigator.navigateToLogin(this, true);
+  }
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    showUsersPresenter = new ShowUsersPresenter(this);
-    showLoadsPresenter = new ShowLoadsPresenter(this);
-    setUpRecyclerView(rvUsers);
-    setUpRecyclerView(rvLoads);
-  }
-
-  private void setUpRecyclerView(RecyclerView recyclerView) {
-    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-  }
-
-  @Override protected void onStart() {
-    super.onStart();
-
-    showUsersPresenter.onViewAttached();
-    showLoadsPresenter.onViewAttached();
+    mNavigator = new Navigator();
   }
 
   @Override protected int getLayoutResource() {
     return R.layout.activity_main;
-  }
-
-  @Override public void showLoading() {
-
-  }
-
-  @Override public void hideLoading() {
-
-  }
-
-  @Override public void renderUsers(List<User> userList) {
-    UsersAdapter usersAdapter = new UsersAdapter(this);
-    usersAdapter.setListOfUsers(userList);
-
-    rvUsers.setAdapter(usersAdapter);
-    usersAdapter.notifyDataSetChanged();
-  }
-
-  @Override public void renderLoads(List<Load> loadsList) {
-    LoadsAdapter loadsAdapter = new LoadsAdapter(this);
-    loadsAdapter.setListOfLoads(loadsList);
-
-    rvLoads.setAdapter(loadsAdapter);
-    loadsAdapter.notifyDataSetChanged();
-
   }
 }
---
+++ presentation/src/main/java/xyz/gonzapico/ontrucktt/ShowUsersActivity.java
@@ -0,0 +1,70 @@
+package xyz.gonzapico.ontrucktt;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import butterknife.BindView;
+import java.util.List;
+import xyz.gonzapico.ontrucktt.showUsers.ShowUsersPresenter;
+import xyz.gonzapico.ontrucktt.showUsers.ShowUsersView;
+import xyz.gonzapico.ontrucktt.showUsers.User;
+import xyz.gonzapico.ontrucktt.showUsers.adapter.UsersAdapter;
+
+public class ShowUsersActivity extends BaseOTActivity implements ShowUsersView {
+  @BindView(R.id.rvUsers) RecyclerView rvUsers;
+  @BindView(R.id.pbLoading) ProgressBar pbLoading;
+  @BindView(R.id.llGlobalShowUsers) LinearLayout llGlobalUsers;
+  private ShowUsersPresenter showUsersPresenter;
+
+  public static Intent getCallingIntent(Context context) {
+    return new Intent(context, ShowUsersActivity.class);
+  }
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    showUsersPresenter = new ShowUsersPresenter(this, ((BaseOTApplication)getApplicationContext()).getFirebaseDatabase());
+    setUpRecyclerView(rvUsers);
+  }
+
+  @Override protected int getLayoutResource() {
+    return R.layout.activity_show_users;
+  }
+
+  @Override public void showLoading() {
+    pbLoading.setVisibility(View.VISIBLE);
+  }
+
+  @Override public void hideLoading() {
+    pbLoading.setVisibility(View.GONE);
+  }
+
+  @Override protected void onStart() {
+    super.onStart();
+
+    showUsersPresenter.onViewAttached();
+  }
+
+  @Override protected void onStop() {
+    super.onStop();
+
+    showUsersPresenter.onViewDetached();
+  }
+
+  @Override public void renderUsers(List<User> userList) {
+    UsersAdapter usersAdapter = new UsersAdapter(this);
+    usersAdapter.setListOfUsers(userList);
+
+    rvUsers.setAdapter(usersAdapter);
+    usersAdapter.notifyDataSetChanged();
+  }
+
+  @Override public void showErrorMessage(String msgError) {
+    Snackbar.make(llGlobalUsers, msgError, Snackbar.LENGTH_SHORT).show();
+  }
+}
